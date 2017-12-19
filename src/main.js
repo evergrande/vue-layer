a24da40f6ecf383379bff2ce4cc09db2bc5d18bf
keep2zero <towaywu@gmail.com>
2017-12-19 16:48:41 +0800

    add popup plugin

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 import jquery from "jquery";
 import layer from "./layer";
 import datepicker from "./datepicker/jquery.jedate";
+import popup from "./popup/popup-plugin";
 import Vue from 'vue';
 
 require('./layer/theme/default/layer.css')
 require('./datepicker/skin/jedate.css')
+require('./popup/popup-plugin.css')
 
 function VueMiddleLayer() {}
   VueMiddleLayer.install = function(Vue, options) {
   Vue.prototype.$layer = layer;
   Vue.prototype.$datepicker = datepicker;
+  Vue.prototype.$popup = popup;
 }
 
 export default VueMiddleLayer;
